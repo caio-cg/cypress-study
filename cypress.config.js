ec58b2a92ee1e949b0112440861b40f4298f1a63
Caio Gava <caio.gava@trustly.com>
2024-06-22 18:24:23 -0300

    Basic scaffold for E2E tests

---
+++ cypress.config.js
@@ -0,0 +1,11 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  projectId: "51jqIi4vz51hs7kYKvAOEP7k",
+
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+});
